cf05c82d825ce8d36b5b9d55f12035e037a954a3
Đào Hồ Anh <daohoanh.work@gmail.com>
2025-05-10 21:19:52 +0700

    Initial commit: Spring Boot project

---
+++ src/main/java/com/example/clothstore/ClothstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clothstore/ClothstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
