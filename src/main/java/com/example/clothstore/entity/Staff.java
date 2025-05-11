19430985dc263f0c611a50a8679bb40f4e5d2b6f
Đào Hồ Anh <daohoanh.work@gmail.com>
2025-05-11 15:04:24 +0700

    Add Staff class in Entity package

---
+++ src/main/java/com/example/clothstore/entity/Staff.java
@@ -0,0 +1,87 @@
+package com.example.clothstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity(name = "staff")
+public class Staff {
+    @Id
+    private String staffId;
+
+    @Column(name = "staff_name")
+    private String staffName;
+
+    @Column(name = "staff_birthday")
+    private Date staffBirthday;
+
+    @Column(name = "staff_phone_number")
+    private String staffPhoneNumber;
+
+    @Column(name = "staff_address")
+    private String staffAddress;
+
+    @Column(name = "staff_password")
+    private String staffPassword;
+
+    @Column(name = "staff_added_date")
+    private Date staffAddedDate;
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public Date getStaffBirthday() {
+        return staffBirthday;
+    }
+
+    public void setStaffBirthday(Date staffBirthday) {
+        this.staffBirthday = staffBirthday;
+    }
+
+    public String getStaffPhoneNumber() {
+        return staffPhoneNumber;
+    }
+
+    public void setStaffPhoneNumber(String staffPhoneNumber) {
+        this.staffPhoneNumber = staffPhoneNumber;
+    }
+
+    public String getStaffAddress() {
+        return staffAddress;
+    }
+
+    public void setStaffAddress(String staffAddress) {
+        this.staffAddress = staffAddress;
+    }
+
+    public String getStaffPassword() {
+        return staffPassword;
+    }
+
+    public void setStaffPassword(String staffPassword) {
+        this.staffPassword = staffPassword;
+    }
+
+    public Date getStaffAddedDate() {
+        return staffAddedDate;
+    }
+
+    public void setStaffAddedDate(Date staffAddedDate) {
+        this.staffAddedDate = staffAddedDate;
+    }
+}
