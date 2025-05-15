b823cb515cfe6e05d9c9deacaf42bc8c2ce700ab
Đào Hồ Anh <daohoanh.work@gmail.com>
2025-05-15 14:37:21 +0700

    Add server.servlet.context-path: /api and remove previous api path

--- src/main/java/com/example/clothstore/controller/HomePageController.java
+++ src/main/java/com/example/clothstore/controller/HomePageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/home-page")
+@RequestMapping("/home-page")
 public class HomePageController {
     @Autowired
     ProductService productService;
